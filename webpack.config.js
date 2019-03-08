b739cd8f579c66d02ea498edae943fdc1942537e
Simon Beckstein <simon.beckstein@gmail.com>
2019-03-08 16:24:42 +0100

    added scss processing

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: ['./src/index.js'],
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'bundle.js'
     },
-    module: {},
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+            }
+        ]
+    },
     plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
         new HTMLWebpackPlugin({
             template: './src/index.html'
         })
